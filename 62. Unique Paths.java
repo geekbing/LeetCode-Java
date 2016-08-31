cae70ed42c949cc7375fdaa00ae8f2e2535b07c3
geekbing <dhuzbb@gmail.com>
2016-08-31 10:22:59 +0800

    新增62题

---
+++ 62. Unique Paths.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m == 1 || n == 1)
+		{
+			return 1;
+		}
+		int[][] ans = new int[m][n];
+		for(int i = 0; i < m; i++)
+		{
+			ans[i][0] = 1;
+		}
+		for(int i = 0; i < n; i ++)
+		{
+			ans[0][i] = 1;
+		}
+		for(int i = 1; i < m; i++)
+		{
+			for(int j = 1; j < n; j++)
+			{
+				ans[i][j] = ans[i - 1][j] + ans[i][j - 1];
+			}
+		}
+		return ans[m - 1][n - 1];
+    }
+}
