83a431aebb4fa789854b914e71d735ff15c43ef7
geekbing <dhuzbb@gmail.com>
2016-09-01 10:07:06 +0800

    新增第343题

---
+++ 343. Integer Break.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int integerBreak(int n) {
+        if(n == 2) {
+			return 1;
+		}
+		if(n == 3) {
+			return 2;
+		}
+		int ans = 1;
+		while(n >= 5) {
+			ans *= 3;
+			n -= 3;
+		}
+		return ans * n;
+    }
+}
