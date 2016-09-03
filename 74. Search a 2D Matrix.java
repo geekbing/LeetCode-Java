b98ea133990550af92dcf9e2618fef9dfe92e7e2
geekbing <dhuzbb@gmail.com>
2016-09-03 10:14:07 +0800

    74题新增方法三

--- 74. Search a 2D Matrix.java
+++ 74. Search a 2D Matrix.java
@@ -87,3 +87,26 @@ public class Solution {
 		return matrix[row][col] == target;
     }
 }
+// 方法三
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if(m == 0) {
+            return false;
+        }
+        int i = 0;
+        int j = matrix[0].length - 1;
+        while(i < m && j >= 0) {
+            if(matrix[i][j] < target) {
+                i++;
+            }
+            else if(matrix[i][j] == target) {
+                return true;
+            }
+            else {
+                j--;
+            }
+        }
+        return false;
+    }
+}
